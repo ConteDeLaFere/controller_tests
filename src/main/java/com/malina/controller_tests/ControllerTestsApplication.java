efe5463e19f2544f7b2c1a426f871edede7952df
Nikita Malinkin <nik.malina20@gmail.com>
2024-11-25 18:24:26 +0300

    Initial commit

---
+++ src/main/java/com/malina/controller_tests/ControllerTestsApplication.java
@@ -0,0 +1,13 @@
+package com.malina.controller_tests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControllerTestsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ControllerTestsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/malina/controller_tests/ControllerTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malina.controller_tests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControllerTestsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
